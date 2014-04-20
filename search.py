bd3e9efe5491e96023664c48ece8525a141217ae
Duncan Townsend <duncant@mit.edu>
2014-04-20 12:48:32 -0400

    Made use of weakmemoize in search.py to make uniform cost search more expressive and efficient

--- search.py
+++ search.py
@@ -2,6 +2,7 @@ from collections import deque
 from itertools import *
 from operator import *
 from heapq import heappush, heappop
+from memoize import weakmemoize
 
 debug = False
 
@@ -15,10 +16,9 @@ class NodeParent(object):
     def expand(self):
         for op in self.operators:
             type(self)(self.successor(op), self, op)
-
-def node_classfactory(successor, is_goal, operators, receiver):
-    namespace = {'__slots__':['state', 'parent', 'op']}
     
+def generic_classfactory(successor, is_goal, operators, receiver, namespace):
+    namespace['__slots__'] = ['state', 'parent', 'op']
     namespace['successor'] = lambda self, op: successor(self.state, op)
     namespace['is_goal'] = property(lambda self: is_goal(self.state))
     namespace['operators'] = property(lambda self: operators(self.state))
@@ -27,7 +27,7 @@ def node_classfactory(successor, is_goal, operators, receiver):
     return type('Node',(NodeParent,),namespace)
 
 def generic_search(schedule_length, add_to_schedule, get_next_from_schedule,
-                   root_state, successor, is_goal, operators):
+                   classfactory, root_state, successor, is_goal, operators):
     def check_visited_and_add(node):
         if debug:
             print "adding:"
@@ -41,7 +41,7 @@ def generic_search(schedule_length, add_to_schedule, get_next_from_schedule,
             n = n.parent
         add_to_schedule(node)
 
-    Node = node_classfactory(successor, is_goal, operators, check_visited_and_add)
+    Node = classfactory(successor, is_goal, operators, check_visited_and_add, {})
     Node(root_state, None, None)
 
     while schedule_length() > 0:
@@ -61,24 +61,37 @@ def generic_search(schedule_length, add_to_schedule, get_next_from_schedule,
 def dfs(root_state, successor, is_goal, operators):
     schedule = list()
     return generic_search(schedule.__len__, schedule.append, schedule.pop,
+                          generic_classfactory,
                           root_state, successor, is_goal, operators)
 
 def bfs(root_state, successor, is_goal, operators):
     schedule = deque()
     return generic_search(schedule.__len__, schedule.append, schedule.popleft,
+                          generic_classfactory,
                           root_state, successor, is_goal, operators)
 
 def ucs(root_state, successor, is_goal, operators, cost):
     schedule = list()
     def add_to_schedule(node):
         # python2's scoping rules are asinine
-        heappush(schedule, (cost(node.state), add_to_schedule.pushcount, node))
+        heappush(schedule, (node.cost, add_to_schedule.pushcount, node))
         add_to_schedule.pushcount += 1
     add_to_schedule.pushcount = 0
     def get_next_from_schedule():
         return heappop(schedule)[-1]
+
+    def classfactory(successor, is_goal, operators, receiver, namespace):
+        namespace['cost'] = property(
+                              weakmemoize(
+                                lambda self:
+                                  cost(self.parent.state
+                                         if self.parent is not None
+                                         else None,
+                                       self.op, self.state) + self.parent.cost))
+        return generic_classfactory(successor, is_goal, operators, receiver, namespace)
+
     return generic_search(schedule.__len__, add_to_schedule, get_next_from_schedule,
-                          root_state, successor, is_goal, operators)
+                          classfactory, root_state, successor, is_goal, operators)
 
 
 __all__ = ['dfs', 'bfs', 'ucs']
