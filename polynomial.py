973bd89ff2913c7654ff5378b97000aa862ce72c
Duncan Townsend <duncant@mit.edu>
2014-05-17 17:58:04 -0400

    Added horner_form method, converts Polynomials to a sequence of Horner's method operations

--- polynomial.py
+++ polynomial.py
@@ -335,4 +335,66 @@ class Polynomial(PolynomialBase, Iterable):
         self.__terms = value
 
 
-__all__ = ['Term', 'Polynomial']
+def horner_form(thing, var=None):
+    if not thing.proper:
+        raise ValueError('Can only put proper Terms and Polynomials into horner form')
+    if isinstance(thing, Term):
+        term = thing
+        del thing
+        if len(term.powers) == 0:
+            return [(term, var)]
+        else:
+            if term.coeff != 1:
+                ops = [(Term(term.coeff), var)]
+            else:
+                ops = []
+            for var, power in term.powers.iteritems():
+                for _ in power:
+                    ops.append((Term(0), var))
+            return ops
+    elif var is None:
+        if not isinstance(thing, Polynomial):
+            # immediate error
+            horner_form(thing, object())
+        poly = thing
+        del thing
+        vars = set()
+        for term in poly:
+            for var in term.powers.iterkeys():
+                vars.add(var)
+        def count_ops(ops):
+            if isinstance(ops, Term):
+                return 1
+            else:
+                retval = 0
+                for op, var in ops:
+                    retval += count_ops(op)
+                return retval
+
+        if len(vars) == 0:
+            assert len(poly) == 1
+            term = iter(poly).next()
+            return horner_form(term, var=None)
+        else:
+            return max(imap(lambda var: horner_form(poly, var), vars),
+                       key=count_ops)
+    elif isinstance(thing, Polynomial):
+        poly = thing
+        del thing
+        if len(poly) == 0:
+            return horner_form(Term(0), var)
+        if len(poly) == 1:
+            term = poly[0]
+            return horner_form(term, var)
+        else:
+            max_power = max(imap(lambda term: term.powers.get(var, 0), poly.terms))
+            coeffs = [Polynomial(Term(0))]*(max_power+1)
+            for term in poly:
+                power = term.powers.get(var, 0)
+                coeffs[power] += term / Term((var, power))
+            ops = [ (horner_form(coeff, var=None), var) for coeff in reversed(coeffs) ]
+            return ops
+    else:
+        raise TypeError("Unknown type %s in horner_form" % repr(type(thing)), thing)
+
+__all__ = ['Term', 'Polynomial', 'horner_form']
