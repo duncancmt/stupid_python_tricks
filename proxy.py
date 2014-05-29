6b629143aed2f4056b500a8c4fe9483a5294e9a8
Duncan Townsend <duncant@mit.edu>
2014-05-28 20:19:50 -0400

    Moved getattr_static to its own file getattr_static.py

--- proxy.py
+++ proxy.py
@@ -1,121 +1,5 @@
-# ============ getattr_static backported from 3.2 ===========
-import types
-
-_sentinel = object()
-
-def _static_getmro(klass):
-    retval = type.__dict__['__mro__'].__get__(klass)
-    if retval is None:
-        retval = tuple()
-    return retval
-
-def _check_instance(obj, attr):
-    instance_dict = {}
-    try:
-        instance_dict = object.__getattribute__(obj, "__dict__")
-    except AttributeError:
-        pass
-    return dict.get(instance_dict, attr, _sentinel)
-
-
-def _check_class(klass, attr):
-    for entry in _static_getmro(klass):
-        if _shadowed_dict(type(entry)) is _sentinel:
-            try:
-                return entry.__dict__[attr]
-            except KeyError:
-                pass
-    return _sentinel
-
-def _is_type(obj):
-    try:
-        _static_getmro(obj)
-    except TypeError:
-        return False
-    return True
-
-def _shadowed_dict(klass):
-    dict_attr = type.__dict__["__dict__"]
-    for entry in _static_getmro(klass):
-        try:
-            class_dict = dict_attr.__get__(entry)["__dict__"]
-        except KeyError:
-            pass
-        else:
-            if not (type(class_dict) is types.GetSetDescriptorType and
-                    class_dict.__name__ == "__dict__" and
-                    class_dict.__objclass__ is entry):
-                return class_dict
-    return _sentinel
-
-def getattr_static(obj, attr, default=_sentinel):
-    """Retrieve attributes without triggering dynamic lookup via the descriptor
-    protocol, __getattr__ or __getattribute__.
-
-    Note: this function may not be able to retrieve all attributes that getattr
-    can fetch (like dynamically created attributes) and may find attributes that
-    getattr can't (like descriptors that raise AttributeError). It can also
-    return descriptor objects instead of instance members in some cases. See the
-    documentation for details.
-    """
-    instance_result = _sentinel
-    if not _is_type(obj):
-        klass = type(obj)
-        dict_attr = _shadowed_dict(klass)
-        if (dict_attr is _sentinel or
-            type(dict_attr) is types.MemberDescriptorType):
-            instance_result = _check_instance(obj, attr)
-    else:
-        klass = obj
-
-    klass_result = _check_class(klass, attr)
-
-    if instance_result is not _sentinel and klass_result is not _sentinel:
-        if (_check_class(type(klass_result), '__get__') is not _sentinel and
-            _check_class(type(klass_result), '__set__') is not _sentinel):
-            return klass_result
-
-    if instance_result is not _sentinel:
-        return instance_result
-    if klass_result is not _sentinel:
-        return klass_result
-
-    if obj is klass:
-        # for types we check the metaclass too
-        for entry in _static_getmro(type(klass)):
-            if _shadowed_dict(type(entry)) is _sentinel:
-                try:
-                    return entry.__dict__[attr]
-                except KeyError:
-                    pass
-    if default is not _sentinel:
-        return default
-    raise AttributeError('%s object has no attribute %s' % (repr(type(obj).__name__), repr(attr)))
-
-# ============ end getattr_static backported from 3.2 ===========
-
-def hasattr_static(obj, name):
-    try:
-        getattr_static(obj, name)
-        return True
-    except AttributeError:
-        return False
-
-def isdescriptor(obj):
-    return ( hasattr_static(obj, '__get__') or \
-             hasattr_static(obj, '__set__') or \
-             hasattr_static(obj, '__delete__') )
-
-def checkdescriptor(obj, name):
-    """returns the descriptor if name is a descriptor in obj, otherwise raises
-    AttributeError"""
-    desc = getattr_static(obj, name)
-    if isdescriptor(desc):
-        return desc
-    else:
-        raise AttributeError
-
 from abc import ABCMeta
+from getattr_static import getattr_static
 
 # TODO: this doesn't work: `cls.meth(proxy, *args, **kwargs)` when proxy is a proxy for an object of type cls
 class BasicProxy(object):
@@ -361,6 +245,30 @@ class BasicProxy(object):
         ins = object.__new__(objclass)
         objclass.__init__(ins, obj, *args, **kwargs)
         return ins
+
+
+
+def hasattr_static(obj, name):
+    try:
+        getattr_static(obj, name)
+        return True
+    except AttributeError:
+        return False
+
+def isdescriptor(obj):
+    return ( hasattr_static(obj, '__get__') or \
+             hasattr_static(obj, '__set__') or \
+             hasattr_static(obj, '__delete__') )
+
+def checkdescriptor(obj, name):
+    """returns the descriptor if name is a descriptor in obj, otherwise raises
+    AttributeError"""
+    desc = getattr_static(obj, name)
+    if isdescriptor(desc):
+        return desc
+    else:
+        raise AttributeError
+
 
 class DifficultDescriptorProxy(BasicProxy):
     """
