c835f73e24831bce57418fe0bf934d09dbc809a2
Duncan Townsend <duncant@mit.edu>
2014-08-18 00:39:28 -0400

    Gave curry the ability to be a decorator

--- curry.py
+++ curry.py
@@ -1,10 +1,14 @@
 from inspect import getargspec
+from numbers import Integral
+from functools import update_wrapper
 
-class UndefinedArityError(Exception):
-    def __str__(self):
-        return "Tried to automatically curry a function with * or * args"
+class UndefinedArityError(ValueError):
+    pass
 
-def curry(fn, numargs = 0):
+# we can't use decorator_decorator here because curry is not
+# signature-preserving
+
+def curry(fn=None, numargs=0):
     """Currys a function
 Returns a new function that, when called, will return either:
 1) the value of the given function applied to the given arguments (if there are
@@ -22,11 +26,36 @@ fn -> the function to be curried
 numargs = 0 -> the number of arguments that will satisfy the function to be
      curried. If numargs is 0, curry extracts the number of arguments from
      introspection. If numargs is 0 and the function has a * or ** argument,
-     curry will raise a UndefinedArityError."""
+     curry will raise a UndefinedArityError.
+
+curry can be used as a decorator. e.g.
+@curry
+def foo(arg0, arg1, arg2...):
+    ...
+
+However, it has an alternative invocation in the case that a function
+is varidiac or you wish to supply "numargs":
+
+@curry(3)
+def foo(*args, **kwargs):
+    ...
+== or ==
+@curry(numargs=3)
+def foo(*args, **kwargs):
+    ...
+"""
+
+    if fn is None:
+        fn = numargs
+    if isinstance(fn, Integral):
+        def curry_with_numargs(new_fn):
+            return curry(new_fn, fn)
+        return curry_with_numargs
+
     if numargs == 0:
         argspec = getargspec(fn)
-        if not (argspec[1] == None and argspec[2] == None):
-            raise UndefinedArityError
+        if not (argspec[1] is None and argspec[2] is None):
+            raise UndefinedArityError("Tried to automatically curry a function with * or ** args")
         numargs = len(argspec[0])
 
     def curry_helper(*given,**givenkeys):
@@ -44,7 +73,7 @@ numargs = 0 -> the number of arguments that will satisfy the function to be
             else:
                 return fn(*new[:(numargs - numargsgiven)], **newkeys )
 
-        return curried_fn
+        return update_wrapper(curried_fn, fn)
     return curry_helper()
 
 __all__ = ['UndefinedArityError', 'curry']
