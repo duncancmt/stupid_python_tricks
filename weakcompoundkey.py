bb303445f14bf409e5641314724c5a75e1cf1fa4
Duncan Townsend <duncant@mit.edu>
2014-08-18 14:25:54 -0400

    Added license notices to all .py files

--- weakcompoundkey.py
+++ weakcompoundkey.py
@@ -1,3 +1,19 @@
+# This file is part of stupid_python_tricks written by Duncan Townsend.
+#
+# stupid_python_tricks is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# stupid_python_tricks is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+# for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with stupid_python_tricks.  If not, see <http://www.gnu.org/licenses/>.
+
+
 # WARNING. IF USED IMPROPERLY, THE CLASS IN THIS MODULE RESULTS IN SOME PRETTY
 # BIZARRE BEHAVIOR. YOU SHOULD ONLY USE INSTANCES OF WeakCompoundKey or
 # WeakCompoundKeyStrict AS KEYS IN A weakref.WeakKeyDictionary
