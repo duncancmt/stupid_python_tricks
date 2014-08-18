0b0a5f83341e5a20016c8ef24ecfcac284fbe0ad
Duncan Townsend <duncant@mit.edu>
2014-08-18 01:35:33 -0400

    Added decorates to decorator_decorator, a decorator for decorators when you already know what's being decorated and decorator_apply or decorator_decorator are inappropriate. Yo dawg.

--- decorator_decorator.py
+++ decorator_decorator.py
@@ -8,7 +8,8 @@ def decorator_apply(dec, func, args, kwargs):
     """
     return decorator.FunctionMaker.create(
         func, 'return decorated(%(signature)s)',
-        dict(decorated=dec(func, *args, **kwargs)), __wrapped__=func)
+        dict(decorated=dec(func, *args, **kwargs)), __wrapped__=func,
+        addsource=True)
 
 @decorator.decorator
 def decorator_decorator(dec, func, *args, **kwargs):
@@ -18,4 +19,14 @@ def decorator_decorator(dec, func, *args, **kwargs):
     else:
         return dec(func, *args, **kwargs)
 
-__all__ = ['decorator_decorator']
+def decorates(dec, func=None):
+    if not isinstance(func, Callable):
+        def decorates_with_func(new_dec):
+            return decorates(new_dec, dec)
+        return decorates_with_func
+
+    return decorator.FunctionMaker.create(
+        func, 'return decorated(%(signature)s)',
+        dict(decorated=dec, __wrapped__=func))
+
+__all__ = ['decorator_decorator', 'decorates']
